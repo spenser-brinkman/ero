4d940c43c161ef6546057a256baca6f7b7e9c934
spenser-brinkman <brinkman.spenser@gmail.com>
2022-05-01 07:56:11 -0700

    Shorter keyword parameters names

--- lib/ero.rb
+++ lib/ero.rb
@@ -1,6 +1,6 @@
 # Provides .ero and #ero to classes and modules to which this module is included.
 module Ero
-  def ero(message: nil, error: nil)
+  def ero(msg: nil, err: nil)
     class_name = self.is_a?(Module) ? self.name : self.class.name
     separator = self.is_a?(Module) ? '.' : '#'
     # Extract the originating method's name from near the end of the first
@@ -8,10 +8,10 @@ module Ero
     method_name = caller[0][/`.*'/][1..-2].gsub(/.+ in /, '')
 
     failure_to_log =
-      case [!message.nil?, !error.nil?]
-      when [true, true] then "#{message} - #{class_name}#{separator}#{method_name} failed - #{error}"
-      when [true, false] then "#{message} - #{class_name}#{separator}#{method_name} failed"
-      when [false, true] then "#{class_name}#{separator}#{method_name} failed - #{error}"
+      case [!msg.nil?, !err.nil?]
+      when [true, true] then "#{msg} - #{class_name}#{separator}#{method_name} failed - #{err}"
+      when [true, false] then "#{msg} - #{class_name}#{separator}#{method_name} failed"
+      when [false, true] then "#{class_name}#{separator}#{method_name} failed - #{err}"
       when [false, false] then "#{class_name}#{separator}#{method_name} failed"
       end
 
@@ -23,7 +23,7 @@ module Ero
   end
 
   module ClassMethods
-    def ero(message: nil, error: nil)
+    def ero(msg: nil, err: nil)
       class_name = self.is_a?(Module) ? name : self.class.name
       separator = self.is_a?(Module) ? '.' : '#'
       # Extract the originating method's name from near the end of the first
@@ -31,10 +31,10 @@ module Ero
       method_name = caller[0][/`.*'/][1..-2].gsub(/.+ in /, '')
 
       failure_to_log =
-        case [!message.nil?, !error.nil?]
-        when [true, true] then "#{message} - #{class_name}#{separator}#{method_name} failed - #{error}"
-        when [true, false] then "#{message} - #{class_name}#{separator}#{method_name} failed"
-        when [false, true] then "#{class_name}#{separator}#{method_name} failed - #{error}"
+        case [!msg.nil?, !err.nil?]
+        when [true, true] then "#{msg} - #{class_name}#{separator}#{method_name} failed - #{err}"
+        when [true, false] then "#{msg} - #{class_name}#{separator}#{method_name} failed"
+        when [false, true] then "#{class_name}#{separator}#{method_name} failed - #{err}"
         when [false, false] then "#{class_name}#{separator}#{method_name} failed"
         end
 
